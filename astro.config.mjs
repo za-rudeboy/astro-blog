0148ce854e678449b2024543cd09290d77ccb6ae
Rudy Adams <rudyada@gmail.com>
2025-07-06 18:11:41 +0200

    performance enhancements

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://www.rudyadams.com",
   integrations: [mdx(), sitemap(), tailwind()],
 });
